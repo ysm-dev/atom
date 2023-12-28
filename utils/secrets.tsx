e41d5f604c5f89afc071df009f9854f4221b9174
ysm-dev <i@ysm.dev>
2023-12-28 21:25:06 +0900

    update

--- utils/secrets.tsx
+++ utils/secrets.tsx
@@ -4,15 +4,6 @@ import { env as honoENV } from 'hono/adapter'
 // Front
 export const SECRET = process.env.NEXT_PUBLIC_SECRET
 
-// Worker (scheduled)
-// export const GITHUB_PAT =
-
-// Worker & Github Actions
-// export const DATABASE_URL =
-
-// Worker
-// export const DISCORD_BOT_TOKEN =
-
 export const GLOBAL: {
   c: Context
 } = {
@@ -22,7 +13,11 @@ export const GLOBAL: {
 export const env = () => honoENV<ENV>(GLOBAL.c)
 
 type ENV = {
+  SECRET: string
+  // Worker (scheduled)
   GITHUB_PAT: string
+  // Worker & Github Actions
   DATABASE_URL: string
+  // export const DISCORD_BOT_TOKEN =
   DISCORD_BOT_TOKEN: string
 }
