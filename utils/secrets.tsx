a91b2ff5838e7dd545aa99005159f19cb76f2067
ysm-dev <i@ysm.dev>
2024-02-08 02:19:00 +0900

    Update token

--- utils/secrets.tsx
+++ utils/secrets.tsx
@@ -5,7 +5,7 @@ import { isServer } from 'utils/isServer'
 // Front
 export const SECRET = `zH4NRP1HMALxxCFnRZABFA7GOJtzU_gIj02alfL1lva`
 
-export const FEEDLY_TOKEN = `AmVJitV7ImEiOiJGZWVkbHkgRGV2ZWxvcGVyIiwiZSI6MTQzMTkyMzQyNzM4NywiaSI6ImRhNzVmOGMxLTYxNmMtNGYwMC04YTQ1LTc5ZTc3Y2IyZTk5ZSIsInAiOjYsInQiOjEsInYiOiJwcm9kdWN0aW9uIiwidyI6IjIwMTUuOCIsIngiOiJzdGFuZGFyZCJ9:feedlydev`
+export const FEEDLY_TOKEN = `A0ddxitduEHya0UPWSig_ZwTBqgmtz9RvJ0whPxZ77Q_SSYQBJ2nMcCNWEHprHz1Bt6foHTJZThUGrY03yn2lWrcCAqfEmSPj7os_kmx3kP0fpH96xfBpWcP6b2JH6BK6R0ezqnyG34tJhO1XuNyYr2d6InfEU3kj2riXbdhMRtT_qepptX5u8VbWD5ef7GSyM-Hg4NNAAecDDNZ0HY6hQE-5kOk5CMsVmN6-7C8lo7Ndso:feedly`
 
 export const GUILD_ID = `943160510814232576`
 
