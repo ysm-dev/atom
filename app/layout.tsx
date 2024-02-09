f0968e6c29b8f7a365ec6b034ce9e4857dc8067e
ysm-dev <i@ysm.dev>
2024-02-09 15:31:22 +0900

    Disable zoom on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: Props) {
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
+          content="width=device-width, initial-scale=1, viewport-fit=cover, user-scalable=no"
         />
       </head>
       <body className="dark scrollbar-hide">
