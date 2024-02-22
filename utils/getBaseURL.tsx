617f10804b4c7c78fed44dce089e868dd2886ad1
ysm-dev <i@ysm.dev>
2024-02-22 17:07:27 +0900

    Update token

--- utils/getBaseURL.tsx
+++ utils/getBaseURL.tsx
@@ -3,7 +3,7 @@ import { isLocal } from 'utils/isLocal'
 
 export const getBaseURL = () => {
   if (isLocal()) {
-    return `http://localhost:${process.env.PORT ?? 3000}`
+    return `http://localhost:${process.env.PORT ?? 3008}`
   }
 
   return `https://${HOST}`
