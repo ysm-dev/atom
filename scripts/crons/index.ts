e47f3a4626d7e47b5418b14dbd0932942a6a8ded
ysm-dev <i@ysm.dev>
2024-03-14 19:12:39 +0900

    proxy

--- scripts/crons/index.ts
+++ scripts/crons/index.ts
@@ -18,10 +18,10 @@ import { type Channels } from 'server/feeds'
 import { textToBinary } from 'utils/binary'
 import { decodeHTMLEntities } from 'utils/decodeHtml'
 import { getFaviconURI } from 'utils/getFaviconURI'
-import { getServerURL } from 'utils/getServerURL'
 import { isRSS } from 'utils/isRSS'
 import { isURL } from 'utils/isURL'
 import { stringify } from 'utils/json'
+import { PROXY_URL } from 'utils/proxy'
 import { GUILD_ID } from 'utils/secrets'
 import { storage } from 'utils/storage'
 import { toCID } from 'utils/toCID'
@@ -98,7 +98,7 @@ async function main() {
           let res: FetchResponse<any> | null = await fetch(xmlURL!)
             .then((res) => {
               if (!res.ok) {
-                return fetch(`${getServerURL()}/proxy/${xmlURL}`).catch((e) => {
+                return fetch(`${PROXY_URL}${xmlURL}`).catch((e) => {
                   console.error(`Failed to fetch ${url}`, e)
                   return res
                 })
@@ -107,7 +107,7 @@ async function main() {
               }
             })
             .catch((e) => {
-              return fetch(`${getServerURL()}/proxy/${xmlURL}`).catch((e) => {
+              return fetch(`${PROXY_URL}${xmlURL}`).catch((e) => {
                 console.error(`Failed to fetch ${url}`, e)
                 return null
               })
