4a0b6ec41f1c2566f785f8bcbddd284d090c8c76
ysm-dev <i@ysm.dev>
2024-05-08 16:42:30 +0900

    refactor: Update login page to use getBaseURL for redirect URI

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,24 +2,16 @@
 
 import { IconBrandDiscord } from '@tabler/icons-react'
 import { Button } from 'components/ui/button'
-import { isServer } from 'utils/isServer'
+import { getBaseURL } from 'utils/getBaseURL'
 
 const DISCORD_CLIENT_ID = `1170597393272668200`
 
 const REDIRECT_PATH = '/oauth/discord'
 
-const getRedirectURI = () => {
-  if (isServer()) {
-    return REDIRECT_PATH
-  } else {
-    return `${window.location.origin}${REDIRECT_PATH}`
-  }
-}
-
 export default function Page() {
   const href = `https://discord.com/api/oauth2/authorize?${new URLSearchParams({
     client_id: DISCORD_CLIENT_ID,
-    redirect_uri: getRedirectURI(),
+    redirect_uri: `${getBaseURL()}${REDIRECT_PATH}`,
     response_type: 'code',
     // https://discord.com/developers/applications/1125294392031326229/oauth2/url-generator
     permissions: '536870912',
